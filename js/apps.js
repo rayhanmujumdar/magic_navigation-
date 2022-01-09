3e03edddd6f3903d7f234929bc550ad257c371be
rayhan mujumdar <rayhanmujumdar0177@gmail.com>
2022-01-10 01:48:30 +0600

    magic nav added

---
+++ js/apps.js
@@ -0,0 +1,43 @@
+// let list = document.querySelectorAll('.list');
+// function magic(){
+//     list.forEach((rayhan)=>
+//     rayhan.classList.remove('active'));
+//     this.classList.add('active');
+// }
+// list.forEach((rayhan)=>
+// rayhan.addEventListener('click',magic));
+
+// let list = document.querySelectorAll('.list');
+// function click(){
+//     list.forEach((item)=>
+//     item.classList.remove("active"));
+//     this.classList.add('active');
+// }
+// list.forEach((item)=>
+// item.addEventListener('mouseover',click));
+
+let list = document.querySelectorAll('.list');
+function magic(){
+list.forEach((allitem) =>
+allitem.classList.remove('active'))
+this.classList.add('active');
+}
+list.forEach((allitem)=>
+allitem.addEventListener('click',magic));
+// click toggle add javascript
+
+let btn = document.querySelector('.btn');
+let hide = document.querySelector('.hide');
+
+btn.addEventListener('click',function(){
+    if(hide.style.display =='block'){
+        hide.style.display = 'none';
+        btn.style.background = 'green';
+        btn.innerHTML= 'Show'
+    }
+    else{
+        hide.style.display = 'block';
+        btn.style.background = 'tomato';
+        btn.innerHTML= 'Hide'
+    }
+})
